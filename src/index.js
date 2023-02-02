6d262a6131345656dd277a20edbfbc33ec5316b5
tal-dev <talgat.abykeev@gmail.com>
2023-02-01 20:40:24 -0600

    update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './redux/reducers';
+
+const store = configureStore({reducer: rootReducer})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>
 );
 
